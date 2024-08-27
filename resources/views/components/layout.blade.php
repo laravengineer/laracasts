3ac09e648e926762ecc52421b676e3c50f8b2bb0
Eduardo Alexandre <laravengineer@gmail.com>
2024-08-27 17:48:40 -0300

    feat: add props to nav-link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,10 +17,14 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                                <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                                <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                                <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link> 
+                                {{-- Acrescentar o : antes de uma prop informa ao laravel para tratar o valor como uma expressão e não uma string. --}}
+                                {{-- O metodo request()->is() retorna um bool para caso a requisição seja feita ao parâmetro informado. --}}
+                                {{-- <a href="/" class="{{ request()->is('/') ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}} rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a> --}}
+                                {{-- <a href="/about" class="{{ request()->is('about') ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}} rounded-md px-3 py-2 text-sm font-medium">About</a>
+                                <a href="/contact" class="{{ request()->is('contact') ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}} rounded-md px-3 py-2 text-sm font-medium">Contact</a> --}}
                             </div>
                         </div>
                     </div>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,2 +1,20 @@
 {{-- Esta é uma forma de passar atributos para um component, ao declarar um <x-nav-link href=""></x-nav-link>, o href automaticamente passará para essa tag. --}}
-<a {{ $attributes }}>{{ $slot }}</a>
+{{-- <a {{ $attributes }}>{{ $slot }}</a> --}}
+
+@props([
+    'active' => false,
+    'type'   => 'a'
+])
+
+@if ($type === 'a')
+    <a class="{{ $active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}} rounded-md px-3 py-2 text-sm font-medium"
+        aria-current="{{ $active ? "page": "false" }}" {{ $attributes }}> <!--Funciona para leitores de páginas saberem se o link levará para a pasta atual a página atual-->
+        {{ $slot }}
+    </a>
+@else
+    <button class="{{ $active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}} rounded-md px-3 py-2 text-sm font-medium"
+        aria-current="{{ $active ? "page": "false" }}" {{ $attributes }}> <!--Funciona para leitores de páginas saberem se o link levará para a pasta atual a página atual-->
+        {{ $slot }}
+    </button>
+@endif
+
