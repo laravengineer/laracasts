67d7270c36f17c337ca058a9f2fdd06b15926c87
Eduardo Alexandre <laravengineer@gmail.com>
2024-08-29 14:30:42 -0300

    feat: add employer factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,27 +18,32 @@ class UserFactory extends Factory
 
     /**
      * Define the model's default state.
+     * Para usar essa função no tinker por exemplo:
+     * App\Models\User::factory()->create();
+     * App\Models\User::factory(10)->create();
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
         return [
-            'first_name' => fake()->firstName(),
-            'last_name' => fake()->lastName(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name'        => fake()->firstName(),
+            'last_name'         => fake()->lastName(),
+            'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password'          => static::$password ??= Hash::make('password'),
+            'remember_token'    => Str::random(10),
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
+     * Para usar essa função no tinker por exemplo:
+     * App\Models\User::factory()->unverified()->create();
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
